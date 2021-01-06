0d1d12a753e0207353b45bfe9c1a53cac050e200
Viktor Zhurbin <vzhurbin@gmail.com>
2021-01-06 23:06:09 +0300

    set viewport meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { UserProvider } from '@/context/User';
 import { DecksProvider } from '@/context/Decks';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -12,6 +13,12 @@ function MyApp({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <UserProvider>
         <DecksProvider>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+          </Head>
           <Component {...pageProps} />
         </DecksProvider>
       </UserProvider>
