19e068cd73e6439c9117a98a1d2d5f414c5892ba
Viktor Zhurbin <vzhurbin@gmail.com>
2021-01-08 07:16:51 +0300

    wip: tui-editor

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,12 +1,17 @@
 import '@toast-ui/editor/dist/toastui-editor.css';
 import dynamic from 'next/dynamic';
 import { EditorProps } from '@toast-ui/react-editor';
+import { useRef, useState } from 'react';
 // import 'codemirror/lib/codemirror.css';
 // import 'highlight.js/styles/github.css';
 // import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 // import hljs from 'highlight.js';
 
-const Tui: React.ComponentType<EditorProps> = dynamic(
+interface Props extends EditorProps {
+  ref: React.MutableRefObject<any>;
+}
+
+const Tui: React.ComponentType<Props> = dynamic(
   () => import('@toast-ui/react-editor').then((mod) => mod.Editor),
   {
     loading: () => <p>...</p>,
@@ -15,8 +20,21 @@ const Tui: React.ComponentType<EditorProps> = dynamic(
 );
 
 export const Editor = () => {
+  const [html, setHtml] = useState('');
+  const editorRef = useRef(null);
+
+  const handleChange = () => {
+    if (editorRef?.current) {
+      console.log(editorRef?.current);
+      const htmlValue = editorRef.current?.getInstance().getHtml();
+      htmlValue && setHtml(htmlValue);
+    }
+  };
+
   return (
     <Tui
+      ref={editorRef}
+      onChange={handleChange}
       initialValue="hello react editor world!"
       previewStyle="vertical"
       height="70vh"
